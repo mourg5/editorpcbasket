9348e5033afa4f21f8d9575df2c088790168d029
mruizgomez <manuel.ruiz@softcrits.es>
2020-06-09 14:27:31 +0200

    [DISEÑO] -Ahora aparecen las banderas al lado del país/nacionalidad.

---
+++ Editor PCBasket - Mou/Converters/PaisToBanderaConverter.cs
@@ -0,0 +1,27 @@
+﻿using EpcbModel;
+using EpcbUtils;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Editor_PCBasket___Mou.Converters
+{
+	[ValueConversion(typeof(string), typeof(ImageSource))]
+	public class PaisToBanderaConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return DbdatUtils.GetBanderaBitmap((Pais)value);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
